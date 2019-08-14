80d5b2efe8081060a9c8165ba739b4acdeb49d92
Andrew Zajac <ajzajac@icloud.com>
2019-08-14 17:13:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,6 @@ def nyc_pigeon_organizer(data)
    data.each do |key, value|
      value.each do |color, name|
        name.each do |name|
-         
+         if pigeon_list[name] === false 
+           
 end
